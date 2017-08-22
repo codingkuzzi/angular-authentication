b8ba7919c5c7dd1a51888ac92c270d3dbb4e12d7
Anna Kuznetsova <aakuznetsova@gmail.com>
2017-08-22 16:46:06 -0700

    create fire DB

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AngularFireModule } from 'angularfire2'
+
 import { AppComponent } from './app.component';
+import { AuthService} from './providers/auth.service';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBRMBGRVCUqsuDtXiWYi5ohMCr4eGF4Tzs",
+   authDomain: "angular-auth-34879.firebaseapp.com",
+   databaseURL: "https://angular-auth-34879.firebaseio.com",
+   projectId: "angular-auth-34879",
+   storageBucket: "angular-auth-34879.appspot.com",
+   messagingSenderId: "849753583715"
+}
 
 @NgModule({
   declarations: [
@@ -12,9 +24,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [ AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
